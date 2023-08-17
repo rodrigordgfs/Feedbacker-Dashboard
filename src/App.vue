ef0b78efca04b208cde1982ed5ab4bffccee528c
Rodrigo Viegas Rodrigues <rodrigo.viegas@ulbra.inf.br>
2023-08-17 17:56:05 -0300

    Feature: Instalado Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <p class="text-lg font-medium">
+    “Tailwind CSS is the only framework that I've seen scale on large teams.
+    It’s easy to customize, adapts to any design, and the build size is tiny.”
+  </p>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
