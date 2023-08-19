b3b3d095b50348c9c4ef4b747823a3bed0b7969e
Rodrigo Viegas Rodrigues <rodrigo.viegas@ulbra.inf.br>
2023-08-19 10:06:53 -0300

    Feature: Criado filtros do Feedbacks

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -4,21 +4,39 @@
   </div>
 
   <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
-    <h1 class="text-4xl font-black text-center text-gray-800">
-      Feedbacks
-    </h1>
+    <h1 class="text-4xl font-black text-center text-gray-800">Feedbacks</h1>
     <p class="text-lg text-center text-gray-800 font-regular">
       Detalhes de todos os feedbacks recebidos.
     </p>
+  </div>
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-graydark">Listagem</h1>
+        <suspense>
+          <template #default>
+            <filters />
+          </template>
+          <template #fallback>
+            <filters-loading class="mt-8" />
+          </template>
+        </suspense>
+      </div>
+      <div class="px-10 pt-20 col-span-3"></div>
+    </div>
   </div>
 </template>
 
 <script>
 import HeaderLogged from '@/components/HeaderLogged'
+import Filters from './components/Filters'
+import FiltersLoading from './components/FiltersLoading'
 
 export default {
   components: {
-    HeaderLogged
+    HeaderLogged,
+    Filters,
+    FiltersLoading
   }
 }
 </script>
